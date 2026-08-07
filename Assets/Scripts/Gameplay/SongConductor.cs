fdc8957ed757e3b48d56324a1cedcc5282b1e2ed
Hung-8-tuoi <nguyenhung.9a5.nbk@gmail.com>
2026-08-07 21:24:07 +0700

    feat: add DSP-based song timing

---
+++ Assets/Scripts/Gameplay/SongConductor.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class SongConductor : MonoBehaviour
+{
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private double startDelay = 0.5;
+    private double songStartDspTime;
+
+    private bool isSongScheduled;
+
+    public double SongTime
+    {
+        get
+        {
+            if (!isSongScheduled)
+            {
+                return 0.0;
+            }
+
+            return AudioSettings.dspTime - songStartDspTime;
+        }
+    }
+    private void StartSong()
+    {
+        // Schedule slightly ahead so the audio system can prepare playback.
+        songStartDspTime = AudioSettings.dspTime + startDelay;
+        musicSource.PlayScheduled(songStartDspTime);
+        isSongScheduled = true;
+    }
+
+    void Start()
+    {
+        StartSong();
+    }
+
+    void Update()
+    {
+        Debug.Log($"Song Time: {SongTime}");
+    }
+}
